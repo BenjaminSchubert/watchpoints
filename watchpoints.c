24ff6feecc75b6c9b26799ee3e219b84a84eb8ca
Benjamin Schubert <benjamin.schubert@epfl.ch>
2015-07-01 16:45:34 +0200

    stabilized add of watchpoints

--- watchpoints.c
+++ watchpoints.c
@@ -92,36 +92,26 @@ module_exit(watchpoint_exit);
 
 
 struct tracked_pointer_list *get_pointer_list_for_pid(u64 pid) {
-	struct list_head *pos;
+	struct tracked_pid_list *pid_list;
 	
-	list_for_each(pos, &tracked_data.list) {
-		if(list_entry(pos, struct tracked_pid_list, list)->pid
-			== pid) {
-			printk(KERN_INFO "PID IS %llu\n", pid);
-			return list_entry(pos, struct tracked_pid_list, list)->pointers;	
+	list_for_each_entry(pid_list, &tracked_data.list, list) {
+		if(pid_list->pid == pid) {
+			return pid_list->pointers;
 		}
 	}
+
 	return NULL;
 }
 
 struct tracked_pointer_list *get_pointer_from_pointer_list(struct tracked_pointer_list *pointer_list, long ptr) {
 	struct tracked_pointer_list *pointer;
 	
-	for(i = 1; i < 1; i++) { printk("I GOT IT\n"); }
-	if(&pointer_list->list == pointer_list->list.next) printk("THIS IS THE SAME\n");
 	list_for_each_entry(pointer, &pointer_list->list, list) {
-		printk("AM I EMPTY ?\n");
-		/*pointer = list_entry(pos, struct tracked_pointer_list, list);
-		printk("THE POINTER IS %p\n", pointer->ptr);
-		if((long) list_entry(pos, struct tracked_pointer_list, list)->ptr == ptr) {
-			return list_entry(pos, struct tracked_pointer_list, list);
-		}*/
+		if((long) pointer->ptr == ptr) {
+			return pointer;
+		}
 	}
 	
-	if(list_is_singular(&pointer_list->list)) {
-		printk("I AM ALONE\n");
-	}
-	printk("THERE IS NOTHING HERE\n");
 	return NULL;
 }
 
@@ -130,34 +120,24 @@ static void watchpoint_handler(struct perf_event *bp,
 			       struct pt_regs *regs)
 {
 	struct tracked_pointer_list *pointer_list;
-	
 	struct tracked_pointer_list *pointer;
-	/*struct tracked_changes_list *new_change;
-	*/
-	printk("The REAL PTR IS %llx\n", bp->attr.bp_addr);
+	struct tracked_changes_list *new_change;
+
 	pointer_list = get_pointer_list_for_pid(bp->ctx->task->pid);
 	pointer = get_pointer_from_pointer_list(pointer_list, bp->attr.bp_addr);
 	
-	if(!pointer) {
-		printk("ABORT ABORT\n");
-		return;
-	}
-	printk("The pointer is %p\n", pointer->ptr);
-
-	
-/*	new_change = kmalloc(sizeof(*new_change), 0);
+	new_change = kmalloc(sizeof(*new_change), 0);
 	new_change->data = kmalloc(pointer->size, 0);
-	printk(KERN_INFO "Pointer is %ld, size is %ld\n", pointer->ptr, pointer->size);
-	return;
-	copy_from_user(new_change->data, (void *) pointer->ptr, pointer->size);
-	printk(KERN_INFO "WE ARE THERE\n");
-	printk(KERN_DEBUG
-		"Process %d at position %ld, new value: %s\n",
-		bp->ctx->task->pid, pointer->ptr, new_change->data);
 	
-*/	
-}
+	pr_debug("The pointer is %p with size of %ld\n", pointer->ptr, pointer->size);
+	
+	copy_from_user(new_change->data, (void *) pointer->ptr, pointer->size);
+	pr_debug("Process %d at position %p, new value: %s\n",
+		bp->ctx->task->pid, pointer->ptr, new_change->data);
 
+	new_change->data_size = pointer->size;
+	list_add(&(new_change->list), &pointer->list);
+}
 
 static struct perf_event *initialize_breakpoint(struct watchpoint_message data, pid_t pid) {
 	struct perf_event *perf_watchpoint;
@@ -184,42 +164,37 @@ static void add_ptr_entry_to_pid(struct tracked_pid_list *tracked_pid, struct wa
 	struct list_head *pos;
 	int ptr_entry_created = 0;
 
-	if(tracked_pid->pointers) {
-		list_for_each(pos, &tracked_pid->pointers->list) {
-			struct tracked_pointer_list *ptr_entry;
-			ptr_entry = list_entry(pos, struct tracked_pointer_list, list);
-			if(ptr_entry->ptr == ptr.data_ptr) {
-				ptr_entry_created = 1;
-				break;
-			}
+	list_for_each(pos, &tracked_pid->pointers->list) {
+		struct tracked_pointer_list *ptr_entry;
+		ptr_entry = list_entry(pos, struct tracked_pointer_list, list);
+		if(ptr_entry->ptr == ptr.data_ptr) {
+			ptr_entry_created = 1;
+			break;
 		}
 	}
-
+	
 	if(!ptr_entry_created) {
 		struct proc_dir_entry *proc_ptr;
-		struct tracked_changes_list *changes;
 		struct tracked_pointer_list *ptr_entry;
+		struct tracked_changes_list *changes;
 		char ptr_value[2*sizeof(long) + 1];
+		
+		changes = kmalloc(sizeof(*changes), 0);
 		
 		sprintf(ptr_value, "%p", ptr.data_ptr);
 		proc_ptr = proc_create(ptr_value, 0, tracked_pid->entry, &proc_fops);
-		changes = kmalloc(sizeof(*changes), 0);
-		INIT_LIST_HEAD(&changes->list);
 
 		ptr_entry = kmalloc(sizeof(*ptr_entry), 0);
 		ptr_entry->ptr = ptr.data_ptr;
 		ptr_entry->size = ptr.data_size;
 		ptr_entry->entry = proc_ptr;
 		ptr_entry->changes = changes;
+		INIT_LIST_HEAD(&changes->list);
 
-		pr_debug("Watchpoints:added pid entry: %p\n", ptr_entry->ptr);
+		pr_debug("added pid entry: %p\n", ptr_entry->ptr);
 		
-		if(tracked_pid->pointers) {
-			list_add(&(ptr_entry->list), &(tracked_pid->pointers)->list);
-		} else {
-			INIT_LIST_HEAD(&ptr_entry->list);
-			tracked_pid->pointers = ptr_entry;
-		}
+		
+		list_add(&(ptr_entry->list), &tracked_pid->pointers->list);
 	}
 }
 
@@ -234,19 +209,22 @@ static void prepare_proc_entry(struct watchpoint_message ptr) {
 			break;
 		}
 	}
-
+	
 	if(!tracked_pid) {
 		struct proc_dir_entry *pid_entry;
+		struct tracked_pointer_list *pointers;
 		char pid_name[30];
-		
+
 		tracked_pid = kmalloc(sizeof(*tracked_pid), 0);
+		pointers = kmalloc(sizeof(*pointers), 0);
 
 		sprintf(pid_name, "%d", current->pid);
 		pid_entry = proc_mkdir(pid_name, proc_watchpoints);
-		
+
 		tracked_pid->pid = current->pid;
 		tracked_pid->entry = pid_entry;
-		tracked_pid->pointers = NULL;
+		tracked_pid->pointers = pointers;
+		INIT_LIST_HEAD(&pointers->list);
 
 		pr_debug("added pid entry: %d\n", tracked_pid->pid);
 		list_add(&(tracked_pid->list), &tracked_data.list);
@@ -291,7 +269,7 @@ static long watchpoints_ioctl(struct file *file, unsigned int cmd,
 	copy_from_user(&data, (void *) ptr_message, sizeof(data));
 
 
-	printk(KERN_DEBUG "Received from pid %d, ptr %p, size %ld\n", current->pid,
+	pr_debug("Received from pid %d, ptr %p, size %ld\n", current->pid,
 	       data.data_ptr, data.data_size);
 
 	switch (cmd) {
